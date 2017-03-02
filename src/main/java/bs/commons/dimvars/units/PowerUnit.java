19fab00756fdba0e711934ca19e6d78c626290d1
brendan1991 <beshort@ucsc.edu>
2017-03-02 05:05:22 -0800

    Implemented power unit variable

---
+++ src/main/java/bs/commons/dimvars/units/PowerUnit.java
@@ -0,0 +1,86 @@
+package bs.commons.dimvars.units;
+
+import java.util.HashMap;
+
+import bs.commons.dimvars.core.UnitConversionMap;
+import bs.commons.dimvars.core.UnitData;
+import bs.commons.dimvars.core.UnitGroup;
+import bs.commons.dimvars.core.UnitData.Unit;
+import bs.commons.dimvars.core.UnitType;
+import bs.commons.dimvars.values.Power;
+
+public enum PowerUnit implements Unit
+{
+	NANOWATT(
+		"Nanowatt",
+		"nW"),
+	MICROWATT(
+		"Microwatt",
+		"uW"),
+	MILLIWATT(
+		"Milliwatt",
+		"mW"),
+	WATT(
+		"watt",
+		"W"),
+	KILOWATT(
+		"Kilowatt",
+		"kW"),
+	MEGAWATT(
+		"Megawatt",
+		"MW"),
+	GIGAWATT(
+		"Gigawatt",
+		"GW"),
+	TERAWATT(
+		"Gigawatt",
+		"TW");
+
+	private PowerUnit(String unit_name, String unit_abbreviation)
+	{
+		Unit.newUnitDetails(unit_name, unit_abbreviation, this, UnitType.POWER);
+	}
+
+	@Override
+	public UnitConversionMap getConversionMap()
+	{
+
+		UnitConversionMap map = new UnitConversionMap();
+		for (PowerUnit unit : PowerUnit.values())
+		{
+			map.addConversions(unit, getPowerConversionArray(unit.ordinal()));
+		}
+		return map;
+	}
+
+	public Double[] getPowerConversionArray(Integer multiplier_index)
+	{
+		Double[] conversions = new Double[PowerUnit.values().length];
+		for (Integer place = 0; place < conversions.length; place++)
+		{
+			conversions[place] = Math.pow(10, -(place - 2) * 3);
+		}
+
+		return conversions;
+	}
+
+	@Override
+	public UnitGroup getGroup()
+	{
+		// TODO Auto-generated method stub
+		return UnitType.POWER;
+	}
+
+	@Override
+	public TimeUnit getRate()
+	{
+		return null;
+	}
+
+	public static void main(String args[])
+	{
+		Power p = new Power(2.0, PowerUnit.MILLIWATT);
+		System.out.println(p.W());
+		System.out.println(p.kW());
+	}
+}
---
+++ src/main/java/bs/commons/dimvars/values/Power.java
@@ -0,0 +1,158 @@
+package bs.commons.dimvars.values;
+
+import bs.commons.dimvars.core.UnitVal;
+import bs.commons.dimvars.core.UnitData.Unit;
+import bs.commons.dimvars.units.TimeUnit;
+import bs.commons.dimvars.core.DynamicValue;
+import bs.commons.dimvars.units.PowerUnit;
+import bs.commons.dimvars.core.UnitType;
+
+public class Power extends UnitVal
+{
+public Power(Double val,Unit unit)
+{
+super(val,unit,UnitType.POWER,null);
+}
+protected Power(Double val,Unit unit, TimeUnit rate)
+{
+super(val,unit,UnitType.POWER,rate);
+}
+public Power()
+{
+super(0.0,PowerUnit.NANOWATT,UnitType.POWER,null);
+}
+public static DynamicValue<Power> getDynamicPowerValue()
+{
+Power valClass = new Power(0.0,PowerUnit.NANOWATT,null);
+Power derClass = new Power(0.0,PowerUnit.NANOWATT, TimeUnit.SECOND);
+return new DynamicValue<Power>(valClass,derClass);
+}
+public Double nW()
+{
+return getValue(PowerUnit.NANOWATT,TimeUnit.SECOND);
+}
+public void nW(Double val)
+{
+setValue(val,PowerUnit.NANOWATT,TimeUnit.SECOND);
+}
+public Double nW(TimeUnit rate)
+{
+return getValue(PowerUnit.NANOWATT,rate);
+}
+public void nW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.NANOWATT,rate);
+}
+public Double uW()
+{
+return getValue(PowerUnit.MICROWATT,TimeUnit.SECOND);
+}
+public void uW(Double val)
+{
+setValue(val,PowerUnit.MICROWATT,TimeUnit.SECOND);
+}
+public Double uW(TimeUnit rate)
+{
+return getValue(PowerUnit.MICROWATT,rate);
+}
+public void uW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.MICROWATT,rate);
+}
+public Double mW()
+{
+return getValue(PowerUnit.MILLIWATT,TimeUnit.SECOND);
+}
+public void mW(Double val)
+{
+setValue(val,PowerUnit.MILLIWATT,TimeUnit.SECOND);
+}
+public Double mW(TimeUnit rate)
+{
+return getValue(PowerUnit.MILLIWATT,rate);
+}
+public void mW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.MILLIWATT,rate);
+}
+public Double W()
+{
+return getValue(PowerUnit.WATT,TimeUnit.SECOND);
+}
+public void W(Double val)
+{
+setValue(val,PowerUnit.WATT,TimeUnit.SECOND);
+}
+public Double W(TimeUnit rate)
+{
+return getValue(PowerUnit.WATT,rate);
+}
+public void W(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.WATT,rate);
+}
+public Double kW()
+{
+return getValue(PowerUnit.KILOWATT,TimeUnit.SECOND);
+}
+public void kW(Double val)
+{
+setValue(val,PowerUnit.KILOWATT,TimeUnit.SECOND);
+}
+public Double kW(TimeUnit rate)
+{
+return getValue(PowerUnit.KILOWATT,rate);
+}
+public void kW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.KILOWATT,rate);
+}
+public Double MW()
+{
+return getValue(PowerUnit.MEGAWATT,TimeUnit.SECOND);
+}
+public void MW(Double val)
+{
+setValue(val,PowerUnit.MEGAWATT,TimeUnit.SECOND);
+}
+public Double MW(TimeUnit rate)
+{
+return getValue(PowerUnit.MEGAWATT,rate);
+}
+public void MW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.MEGAWATT,rate);
+}
+public Double GW()
+{
+return getValue(PowerUnit.GIGAWATT,TimeUnit.SECOND);
+}
+public void GW(Double val)
+{
+setValue(val,PowerUnit.GIGAWATT,TimeUnit.SECOND);
+}
+public Double GW(TimeUnit rate)
+{
+return getValue(PowerUnit.GIGAWATT,rate);
+}
+public void GW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.GIGAWATT,rate);
+}
+public Double TW()
+{
+return getValue(PowerUnit.TERAWATT,TimeUnit.SECOND);
+}
+public void TW(Double val)
+{
+setValue(val,PowerUnit.TERAWATT,TimeUnit.SECOND);
+}
+public Double TW(TimeUnit rate)
+{
+return getValue(PowerUnit.TERAWATT,rate);
+}
+public void TW(Double val, TimeUnit rate)
+{
+setValue(val,PowerUnit.TERAWATT,rate);
+}
+}
